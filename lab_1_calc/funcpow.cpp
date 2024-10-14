427c1043df83c6fa71f8f4d3beb05f6ce2c2843e
Egor Grebenkin <gregorru@outlook.com>
2024-10-15 02:42:39 +0300

    удалил лишние файлы

--- lab_1_calc/funcpow.cpp
+++
@@ -1,9 +0,0 @@
-#define EXPORT extern "C" __declspec(dllexport)
-
-#include <cmath>
-
-// Ёкспортируема€ функци€ возведени€ в степень
-// ѕервый аргумент Ч основание (base), второй Ч показатель (exponent)
-EXPORT double func(double base, double exponent) {
-    return std::pow(base, exponent);
-}
--- lab_1_calc/funcsin.cpp
+++
@@ -1,8 +0,0 @@
-#define EXPORT extern "C" __declspec(dllexport)
-
-#include <cmath>
-
-// Экспортируемая функция sin
-EXPORT double func(double x) {
-    return std::sin(x);
-}
